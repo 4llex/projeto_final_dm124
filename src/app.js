afcdf09c12678671c16d08d36e852440b4013a96
Alex Rosa <alexr@gec.inatel.br>
2019-12-03 22:01:59 -0300

    added post and get methods

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const app = express();
 
-app.use((request, response, next) => {
+const tasksRouter = require('./routes/tasks');
+app.use('/api/tasks', tasksRouter);
+
+
+/* app.use((request, response, next) => {
   const data = {
-    message: "Colocando servidor no ar!!! Funcionou ... GOD BLESS!",
+    message: "Colocando SERVIDOR no ar!!! Funcionou ... GOD BLESS!",
   }
 
   response
     .status(200)
     .json(data)
-});
+});*/
 
-module.exports = app;
+module.exports = app; 
---
+++ src/routes/tasks.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/', (request, response) => {
+ response.status(200).json({
+   message: 'Handling POST requests para pedidios',
+   id: "1",
+   id_do_pedido: "1",
+   id_clinte: "1",
+   nome_recebidor: "Joao",
+   cpf_recebidor: "09812308",
+   recebidor_comprador: true,
+   data: "xx:xx",
+   localizacao: "wwww"
+   
+ });
+});
+
+router.get('/', (request, response) => {
+    const data = {};
+    data.message = `Handling ${request.protocol} ${request.method} para buscar pedidios`;
+    
+    response
+      .status(200)
+      .json(data)
+  });
+
+module.exports = router;
